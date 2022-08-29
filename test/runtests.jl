ba0e0ece2fa7d37edf8f7a86a42086cf72e6a517
Jake Ireland <jakewilliami@icloud.com>
2022-08-29 18:42:42 +1200

    Refactor tests into system-specific branches
    This will enable us to develop tests for all OSes (addresses #2)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,22 @@
 using HiddenFiles
 using Test
 
-@testset "HiddenFiles.jl" begin
-    @test HiddenFiles._isinvisible("/Volumes")
-    @test ishidden("/Volumes")
-    @test !HiddenFiles._isinvisible("($(homedir()))/.bashrc")
-    @test ishidden("$(homedir())/.bashrc")
-    @test !ishidden("$(homedir())/Desktop")
+@static if Sys.isunix()
+    @static if Sys.isapple()
+        @testset "HiddenFiles.jl—macOS" begin
+            @test HiddenFiles._isinvisible("/Volumes")
+            @test ishidden("/Volumes")
+            @test !HiddenFiles._isinvisible("($(homedir()))/.bashrc")
+            @test ishidden("$(homedir())/.bashrc")
+            @test !ishidden("$(homedir())/Desktop")
+        end
+    else
+        @testset "HiddenFiles.jl—General UNIX" begin end
+    end
+elseif Sys.iswindows()
+    @testset "HiddenFiles.jl—Windows" begin end
+else
+    @testset "HiddenFiles.jl—Else branch (invalid OS)" begin end
 end
+
+
