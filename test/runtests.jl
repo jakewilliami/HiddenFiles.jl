7af0be2644a11a2424f1fdbda570125f80c8ce44
Jake Ireland <jakewilliami@icloud.com>
2022-08-30 08:19:50 +1200

    Implement @test_throws for nonexistent files
    Also move all tests into a global testset so that different OS
    branches are contained within subtests.  In cases such as macOS, both
    the UNIX branch and the macOS branch of testing will be used.
    
    Addresses #2.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,49 @@
 using HiddenFiles
 using Test
-
-@static if Sys.isunix()
-    @static if Sys.isapple()
-        @testset "HiddenFiles.jl—macOS" begin
-            # Case 1: Dot directories and files
+@testset "HiddenFiles.jl" begin
+    @static if Sys.isunix()
+        @testset "HiddenFiles.jl—General UNIX" begin
             @test ishidden("$(homedir())/.bashrc")
             @test !ishidden("$(homedir())/Desktop")
-            
-            # Case 2: UNIX-specific directories
+            @test_throws Base.IOError HiddenFiles.ishidden("~/.bashrc")
+            @test HiddenFiles.ishidden(expanduser("~/.bashrc"))
+        end
+        
+        @static if Sys.isapple()
+            @testset "HiddenFiles.jl—macOS" begin
+                # Case 1: Dot directories and files
+                @test ishidden("$(homedir())/.bashrc")
+                @test !ishidden("$(homedir())/Desktop")
+                
+                # Case 2: UNIX-specific directories
+                # TODO: complete this case
+                @test HiddenFiles.ishidden("/bin/")
+                
+                # Case 3: Explicitly hidden files and directories
+                @test HiddenFiles._isinvisible("/Volumes")
+                @test ishidden("/Volumes")
+                @test !HiddenFiles._isinvisible("$(homedir())/.bashrc")
+                
+                # Case 4: Packages and bundles
+                @test !ishidden("/System/Applications/Utilities/Terminal.app")
+                @test ishidden("/System/Applications/Utilities/Terminal.app/Contents")
+                @test ishidden("/System/Applications/Utilities/Terminal.app/Contents/")  # This should be the same as above, as we expand all paths using realpath
+                @test !HiddenFiles._ispackage_or_bundle("/System/Applications/Utilities/Terminal.app/Contents/")
+                @test HiddenFiles._exists_inside_package_or_bundle("/System/Applications/Utilities/Terminal.app/Contents/")
+                @test !HiddenFiles._exists_inside_package_or_bundle("/bin/")
+            end
+        else
+            @testset "HiddenFiles.jl—UNIX excluding macOS" begin
+                # TODO
+            end
+        end
+    elseif Sys.iswindows()
+        @testset "HiddenFiles.jl—Windows" begin
             # TODO
-            
-            # Case 3: Explicitly hidden files and directories
-            @test HiddenFiles._isinvisible("/Volumes")
-            @test ishidden("/Volumes")
-            @test !HiddenFiles._isinvisible("($(homedir()))/.bashrc")
-            
-            # Case 4: Packages and bundles
-            @test !ishidden("/System/Applications/Utilities/Terminal.app")
-            @test ishidden("/System/Applications/Utilities/Terminal.app/Contents")
-            @test ishidden("/System/Applications/Utilities/Terminal.app/Contents/")  # This should be the same as above, as we expand all paths using realpath
-            @test !HiddenFiles._ispackage_or_bundle("/System/Applications/Utilities/Terminal.app/Contents/")
-            @test HiddenFiles._exists_inside_package_or_bundle("/System/Applications/Utilities/Terminal.app/Contents/")
         end
     else
-        @testset "HiddenFiles.jl—General UNIX" begin
+        @testset "HiddenFiles.jl—Else branch (invalid OS)" begin
             # TODO
         end
     end
-elseif Sys.iswindows()
-    @testset "HiddenFiles.jl—Windows" begin
-        # TODO
-    end
-else
-    @testset "HiddenFiles.jl—Else branch (invalid OS)" begin
-        # TODO
-    end
 end
-
-
