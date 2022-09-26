32a356c354ed23beb07c59a463d62c34bfa9d14d
Jake Ireland <jakewilliami@icloud.com>
2022-09-26 18:40:03 +1300

    Add condition for SystemError for older versions of Julia

--- src/HiddenFiles.jl
+++ src/HiddenFiles.jl
@@ -193,7 +193,9 @@ function ishidden(f::AbstractString)
     try
         rp = realpath(f)
     catch e
-        isa(e, Base.IOError) && throw(Base.uv_error("ishidden($(repr(f)))", e.code))
+        err_prexif = "ishidden($(repr(f)))"
+        isa(e, SystemError) && throw(Base.uv_error(err_prexif, e.errno))  # Julia 1.0 throws a SystemError when `realpath` fails
+        isa(e, Base.IOError) && throw(Base.uv_error(err_prexif, e.code))
         rethrow()
     end
     return _ishidden(f, realpath(f))
