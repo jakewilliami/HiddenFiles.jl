d715d975c2caaaf23b2dde2a17ca96b711555809
Jake Ireland <jakewilliami@icloud.com>
2022-09-27 01:40:55 +1300

    WIP: select iszfs function depending on Julia version
    I also finished cleaning up the macOS and BSD versions of iszfs.
    Interestingly, macOS appears to have both f_type and f_fssubtype
    fields set from statfs (despite the latter not actually being
    documented.
    
    I am still deciding whether or not our < 1.6 version is a good idea.

--- src/utils/zfs.jl
+++ src/utils/zfs.jl
@@ -1,26 +1,3 @@
-# Adapted from Julia 1.8's diskstat: https://github.com/JuliaLang/julia/pull/42248
-# C calls for UV statfs requires Julia 1.6
-# http://docs.libuv.org/en/v1.x/fs.html#c.uv_fs_statfs
-struct DiskStat
-    ftype::UInt64
-    bsize::UInt64
-    blocks::UInt64
-    bfree::UInt64
-    bavail::UInt64
-    files::UInt64
-    ffree::UInt64
-    fspare::NTuple{4, UInt64} # reserved
-end
-function statfs(f::AbstractString)
-    buf = Vector{UInt8}(undef, ccall(:jl_sizeof_uv_fs_t, Int32, ()))
-    i = ccall(:uv_fs_statfs, Cint, (Ptr{Cvoid}, Ptr{Cvoid}, Cstring, Ptr{Cvoid}),
-                C_NULL, buf, f, C_NULL)
-    i < 0 && Base.uv_error("statfs($(repr(f)))", i)
-    p = ccall(:jl_uv_fs_t_ptr, Ptr{UInt32}, (Ptr{Cvoid},), buf)
-
-    return unsafe_load(reinterpret(Ptr{DiskStat}, p))
-end
-
 # https://lists.freebsd.org/pipermail/freebsd-hackers/2018-February/052295.html
 const SUN_ZFS_SUPER_MAGIC = 0x1b
 const BSD_ZFS_SUPER_MAGIC = 0xde  # Obtained from testing
@@ -28,12 +5,6 @@ const BSD_ZFS_SUPER_MAGIC = 0xde  # Obtained from testing
 const LINUX_ZFS_SUPER_MAGIC = 0x2fc12fc1
 
 const ZFS_SUPER_MAGICS = (SUN_ZFS_SUPER_MAGIC, BSD_ZFS_SUPER_MAGIC, LINUX_ZFS_SUPER_MAGIC)
-
-function iszfs(f::AbstractString)
-    s = statfs(f)
-    return s.ftype ∈ ZFS_SUPER_MAGICS
-end
-
 
 #=
 #include <stdio.h>
@@ -48,49 +19,81 @@ int main() {
 	printf("st.f_type: 0x%llx\n", (uint64_t) st.f_type);
     return 0;
 }
-
 =#
 
-@static if Sys.isapple()
-    # macOS: https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/statfs.2.html
-
-    const SIZEOF_STATFS = 2168
-    const SIZEOF_STATFS64 = 2168
-    const MACOS_F_OTYPE_OFFSET = 13  # 
-
-    function iszfs()
-        # 0x1a <- offset 69 in UInt8 array in statfs, 53 in UInt8 array in statfs64
-        # error("not yet implemented")
-        buf = zeros(UInt8, SIZEOF_STATFS)  ## 64
-        ccall(:statfs, Int, (Cstring, Ptr{Cvoid}), "/", buf)  # buf[0x0d] => 0x00
-        # return buf
-        
-        buf = zeros(UInt8, SIZEOF_STATFS64)  ## 32
-        ccall(:statfs64, Int, (Cstring, Ptr{Cvoid}), "/", buf) # buf[0x13] => 0x73667061
-        return buf
+@static if VERSION ≥ v"1.6"
+    @static if VERSION < v"1.8"
+        # Adapted from Julia 1.8's diskstat: https://github.com/JuliaLang/julia/pull/42248
+        # C calls for UV statfs requires Julia 1.6
+        # http://docs.libuv.org/en/v1.x/fs.html#c.uv_fs_statfs
+        struct DiskStat
+            ftype::UInt64
+            bsize::UInt64
+            blocks::UInt64
+            bfree::UInt64
+            bavail::UInt64
+            files::UInt64
+            ffree::UInt64
+            fspare::NTuple{4, UInt64} # reserved
+        end
+        function statfs(f::AbstractString)
+            buf = Vector{UInt8}(undef, ccall(:jl_sizeof_uv_fs_t, Int32, ()))
+            i = ccall(:uv_fs_statfs, Cint, (Ptr{Cvoid}, Ptr{Cvoid}, Cstring, Ptr{Cvoid}),
+                      C_NULL, buf, f, C_NULL)
+            i < 0 && Base.uv_error("statfs($(repr(f)))", i)
+            p = ccall(:jl_uv_fs_t_ptr, Ptr{UInt32}, (Ptr{Cvoid},), buf)
+            
+            return unsafe_load(reinterpret(Ptr{DiskStat}, p))
+        end
+    else
+        statfs = diskstat
     end
-elseif Sys.isbsd()
-    const SIZEOF_STATFS = 2344
-
-    # BSD: https://www.freebsd.org/cgi/man.cgi?query=statfs&sektion=2
-    const BSD_F_TYPE_OFFSET = 0x03  # 2 in UInt64, but 3 in UInt32
-
-    function iszfs()
-        # 0xde <- offset 5 in UInt8 array
-        # error("not yet implemented")
-        buf = zeros(UInt8, SIZEOF_STATFS)  ## 32
-        ccall(:statfs, Int, (Cstring, Ptr{Cvoid}), "/", buf)  # buf[0x03] => 0x10001010
-        return buf
-    end
-elseif Sys.isunix()
-    # Linux: https://man7.org/linux/man-pages/man2/statfs.2.html
-    function iszfs()
-        error("not yet implemented")
-        buf = zeros(UInt8, error("NEED TO TEST sizeof(struct statfs) ON LINUX"))  ## 32
-        ccall(:statfs, Int, (Cstring, Ptr{Cvoid}), "/", buf)  # 
-        return buf
+    
+    function iszfs(f::AbstractString)
+        s = statfs(f)
+        return s.ftype ∈ ZFS_SUPER_MAGICS
     end
 else
-    iszfs() = error("Cannot call statfs for non-unix operating systems")
+    # If Julia version < 1.6, we have to write our own, not very nice solution
+    @static if Sys.isapple()
+        # macOS: https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/statfs.2.html
+        const SIZEOF_STATFS = 2168
+        const F_TYPE_OFFSET = 0x3d
+        const F_FSSUBTYPE_OFFSET = 0x45
+        
+        function iszfs(f::AbstractString)
+            buf = Vector{UInt8}(undef, SIZEOF_STATFS)
+            # statfs(const char *path, struct statfs *buf);
+            i = ccall(:statfs, Int, (Cstring, Ptr{Cvoid}), f, buf)
+            i < 0 && Base.uv_error("statfs($(repr(f)))", i)
+            return buf[F_TYPE_OFFSET] ∈ ZFS_SUPER_MAGICS || buf[F_FSSUBTYPE_OFFSET] ∈ ZFS_SUPER_MAGICS
+        end
+    elseif Sys.isbsd()
+        # https://www.freebsd.org/cgi/man.cgi?query=statfs&sektion=2
+        const SIZEOF_STATFS = 2344
+        const F_TYPE_OFFSET = 0x05
+        
+        function iszfs(f::AbstractString)
+            buf = Vector{UInt8}(undef, SIZEOF_STATFS)
+            # statfs(const char *path, struct statfs *buf);
+            i = ccall(:statfs, Int, (Cstring, Ptr{Cvoid}), f, buf)
+            i < 0 && Base.uv_error("statfs($(repr(f)))", i)
+            return buf[F_TYPE_OFFSET] ∈ ZFS_SUPER_MAGICS
+        end
+    elseif Sys.isunix()
+        # Linux: https://man7.org/linux/man-pages/man2/statfs.2.html
+        const SIZEOF_STATFS = error("not yet implemented")
+        const F_TYPE_OFFSET = error("not yet implemented")
+        
+        function iszfs(f::AbstractString)
+            buf = zeros(UInt8, SIZEOF_STATFS)
+            # statfs(const char *path, struct statfs *buf);
+            i = ccall(:statfs, Int, (Cstring, Ptr{Cvoid}), f, buf)
+            i < 0 && Base.uv_error("statfs($(repr(f)))", i)
+            return buf[F_TYPE_OFFSET] ∈ ZFS_SUPER_MAGICS
+        end
+    else
+        iszfs() = error("Cannot call statfs for non-Unix operating systems")
+    end
 end
 
